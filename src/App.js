ca97140caf381ea78aeaca36332ed170d947a079
DemetriusBS <dbsfox@outlook.com>
2021-05-26 15:01:02 -0300

    Implementação do logo

--- src/App.js
+++ src/App.js
@@ -3,6 +3,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
+      <div className='logo'>
+         <span className='logoIniciais'>DBS</span>
+      </div>
+
+      <div className='quemSou'>
+
+      </div>
+
+      <div className='habilidades'>
+
+      </div>
+
+      <footer className='rodape'>
+
+      </footer>
      
     </div>
   );
