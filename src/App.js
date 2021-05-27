670c0f240289c8b786ce2211c65e820abd51af7f
DemetriusBS <dbsfox@outlook.com>
2021-05-27 09:53:37 -0300

    Implementação - Quem sou - Provisório

--- src/App.js
+++ src/App.js
@@ -9,7 +9,14 @@ function App() {
       </div>
 
       <div className='quemSou'>
-
+         <span className ='textoApresentacao'>
+           Olá, meu nome é Demetrius Batista dos Santos! Sou estudante de Análise e Desenvolvimento de Sistemas
+           e estou no último período. Meu interesse pela área de TI é fruto da minha curiosidade, minha esposa 
+           é Engenheira de Software e vendo ela trabalhar ficava me perguntando se eu era capaz de fazer o mesmo.
+           Pois bem, dei os primeiros passos e não parei mais, aqui estou eu, praticando e graças a minha 
+           curiosidade, prestes a conquistar meu diploma! Estou à procura de uma oportunidade para colocar em 
+           prática o que já sei e aprender ainda mais, alçando voos cada vez maiores!
+         </span>
       </div>
 
       <div className='habilidades'>
